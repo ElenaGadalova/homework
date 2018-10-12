97f971c863c79a9b09ae3ca852e959d85ee660fb
ElenaGadalova <mrsgadalova@gmail.com>
2018-10-12 21:02:37 +0300

    amountOfElementsToScan

--- src/Sum.java
+++ src/Sum.java
@@ -2,13 +2,24 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter 5 numbers ");
+        System.out.println("Enter a positive number");
+
+        int amountOfElementsToScan = in.nextInt();
+
+        if (amountOfElementsToScan <=0) {
+            System.out.println("That not a positive number!");
+            return;
+        }
+
+        System.out.println("Enter" + amountOfElementsToScan + "numbers");
         int sum = 0;
-        for (int i = 0; i <5; i++) {
+        for (int i = 0; i <amountOfElementsToScan; i++) {
             sum = sum + in.nextInt();
         }
         System.out.println(sum);
     }
+
 }
